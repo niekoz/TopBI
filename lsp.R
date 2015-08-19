04080d6697e7cb46f11bb3b516cafbd7ef3190cf
Ewoud Westerbaan <ewoud.westerbaan@topbi.nl>
2015-08-19 13:40:14 +0200

    added description of lsp.R

--- lsp.R
+++ lsp.R
@@ -1,9 +1,11 @@
-lsp <- function(package, all.names = FALSE, pattern) 
+# This method shows all methods of a package.
+
+lsp <- function(package, all.names = FALSE, pattern)
 {
   package <- deparse(substitute(package))
   ls(
-    pos = paste("package", package, sep = ":"), 
-    all.names = all.names, 
+    pos = paste("package", package, sep = ":"),
+    all.names = all.names,
     pattern = pattern
   )
 }
